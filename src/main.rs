62fb163eeda652593604bd4e85a11465a8a517b4
codygeek01 <s474996633@gmail.com>
2024-06-22 23:39:53 +0530

    handling input

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
-use std::{sync::{Arc, Mutex}, thread};
-
 mod ui;
 mod env;
 mod shell;
 mod constants;
 
 fn main() -> Result<(), eframe::Error> {
-    let default_shell = env::read_env(constants::SHELL);
+    let default_shell = env::read_env(constants::SHELL);    
     let stdout_fd = shell::spawn_pty_with_shell(default_shell);
-    let shared_buffer: Arc<Mutex<Vec<u8>>> = Arc::new(Mutex::new(Vec::new()));
-    let shared_buffer_clone = shared_buffer.clone();
 
-    thread::spawn(move || {
-        while let Some(read_bytes) = shell::read_from_fd(&stdout_fd) {
-            let mut buffer = shared_buffer_clone.lock().unwrap();
-            buffer.extend_from_slice(&read_bytes);
-        }
-    });
-
-    ui::run(shared_buffer)?;
+    ui::run(stdout_fd)?;
     Ok(())
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,32 +1,63 @@
-use std::sync::{Arc, Mutex};
+use std::os::fd::{AsFd, AsRawFd, OwnedFd};
 use eframe::{egui, Result};
-use crate::constants;
+use crate::{constants, shell};
+use nix::fcntl::{fcntl, FcntlArg, OFlag};
 
 mod util;
 
-pub fn run(shared_buffer: Arc<Mutex<Vec<u8>>>) -> Result<()> {
-    eframe::run_native(constants::OXIDE, util::native_options(), Box::new(|cc| Box::new(Oxide::new(cc, shared_buffer))))
+pub fn run(fd: OwnedFd) -> Result<()> {
+    eframe::run_native(constants::OXIDE, util::native_options(), Box::new(|cc| Box::new(Oxide::new(cc, fd))))
 }
 
-#[derive(Default)]
 struct Oxide {
-    shared_buffer: Arc<Mutex<Vec<u8>>>,
+    buffer: Vec<u8>,
+    fd: OwnedFd,
 }
 
 impl Oxide {
-    fn new(_: &eframe::CreationContext<'_>, shared_buffer: Arc<Mutex<Vec<u8>>>) -> Self {
-        Self { shared_buffer }
+    fn new(_: &eframe::CreationContext<'_>, fd: OwnedFd) -> Self {
+        let flags = fcntl(fd.as_raw_fd(), FcntlArg::F_GETFL).unwrap();
+        let mut flags = OFlag::from_bits(flags & OFlag::O_ACCMODE.bits()).unwrap();
+        flags.set(OFlag::O_NONBLOCK, true);
+        fcntl(fd.as_raw_fd(), FcntlArg::F_SETFL(flags)).unwrap();
+
+        Oxide {
+            buffer: Vec::new(),
+            fd
+        }
     }
 }
 
 impl eframe::App for Oxide {
     fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
-        let buffer = self.shared_buffer.lock().unwrap();
-        let output_text = String::from_utf8_lossy(&buffer);
+
+        if let Some(buf) = shell::read_from_fd(&self.fd) {
+            self.buffer.extend_from_slice(&buf);
+        }
         
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello World!");
-            ui.text_edit_multiline(&mut output_text.to_string());
+            ui.input(|input_state| {
+                for event in &input_state.events {
+
+                    let text = match event {
+                        egui::Event::Text(text) => text,
+                        egui::Event::Key { key: egui::Key::Enter, pressed: true, ..} => "\n",
+                        _ => ""
+                    };
+
+                    let bytes = text.as_bytes();
+                    let mut to_write: &[u8] = bytes;
+                    
+                    while !to_write.is_empty() {
+                        let written = nix::unistd::write(self.fd.as_fd(), to_write).unwrap();
+                        to_write = &to_write[written..];
+                    }
+                }
+            });
+            
+            unsafe{
+                ui.label(std::str::from_utf8_unchecked(&self.buffer));
+            }
         });
     }
 }
